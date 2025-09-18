9b3b9b479f6ea5634e97c59a8565fda0c50d87b1
Easylandings <easylandings.software@gmail.com>
2025-09-17 22:14:33 -0400

    grrrr

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const setViewportHeight = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+
+window.addEventListener('resize', setViewportHeight);
+setViewportHeight();
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
